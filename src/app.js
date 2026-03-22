e6e261e943697eab3871a4299f5d3120358e4337
Helen0105 <marinenkoolena21@gmail.com>
2026-03-22 14:09:02 +0300

    second commit

---
+++ src/app.js
@@ -0,0 +1,9 @@
+// import plus from "./js/calculator" //дефолтний імпорт
+// console.log(plus(3, 8));
+
+// import {minus} from "./js/calculator";
+// console.log(minus(20, 15));
+
+// import calc from "./js/calculator"
+// console.log(calc.add(10, 2));
+// console.log(calc.minus(25, 15));
---
+++ src/js/calculator.js
@@ -0,0 +1,19 @@
+// export default function add(a, b){ //дефолтний експорт 
+//     return a + b 
+// }
+
+// export function minus(a, b){ //іменований експорт
+// return a - b
+// }
+
+
+// function minus(a, b){ 
+//  return a - b
+//  }
+
+
+//  function add(a, b){ 
+//     return a + b 
+// }
+
+// export default {minus, add}
